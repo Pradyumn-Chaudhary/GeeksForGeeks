6a4e97207119bc7261d929d927854610ddb5fd02
Pradyumn Chaudhary <155794678+Pradyumn-Chaudhary@users.noreply.github.com>
2024-11-25 08:10:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            String[] inputLine = br.readLine().split(" ");
+            int n = inputLine.length;
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            System.out.println(new Solution().maxProduct(arr));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    int maxProduct(int[] arr) {
+        int max = arr[0];
+        int min = arr[0];
+        int result = arr[0];
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                int temp = max;
+                max = min;
+                min = temp;
+            }
+            max = Math.max(arr[i], max * arr[i]);
+            min = Math.min(arr[i], min * arr[i]);
+            result = Math.max(result, max);
+        }
+        return result;
+    }
+}
