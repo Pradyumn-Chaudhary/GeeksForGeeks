cefba0b886c59e947881a505747557ab94ce4468
Pradyumn Chaudhary <155794678+Pradyumn-Chaudhary@users.noreply.github.com>
2025-01-03 13:13:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        while (tc-- > 0) {
+            String s[] = br.readLine().split(" ");
+            int arr[] = new int[s.length];
+
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = Integer.parseInt(s[i]);
+            }
+            int k = Integer.parseInt(br.readLine());
+
+            Solution obj = new Solution();
+            System.out.println(obj.subarrayXor(arr, k));
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+class Solution {
+    public long subarrayXor(int a[], int k) {
+        // code here
+        int n = a.length; //size of the given array.
+        int xr = 0;
+        Map<Integer, Integer> mpp = new HashMap<>(); //declaring the map.
+        mpp.put(xr, 1); //setting the value of 0.
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            // prefix XOR till index i:
+            xr = xr ^ a[i];
+
+            //By formula: x = xr^k:
+            int x = xr ^ k;
+
+            // add the occurrence of xr^k
+            // to the count:
+            if (mpp.containsKey(x)) {
+                cnt += mpp.get(x);
+            }
+
+            // Insert the prefix xor till index i
+            // into the map:
+            if (mpp.containsKey(xr)) {
+                mpp.put(xr, mpp.get(xr) + 1);
+            } else {
+                mpp.put(xr, 1);
+            }
+        }
+        return cnt;
+    }
+}
+
+
+
+
+
+
