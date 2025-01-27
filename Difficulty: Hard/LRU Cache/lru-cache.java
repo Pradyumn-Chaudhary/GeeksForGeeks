e995272a64f043ba53db73f71c55e499a95d358c
Pradyumn Chaudhary <155794678+Pradyumn-Chaudhary@users.noreply.github.com>
2025-01-27 10:06:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/LRU Cache/lru-cache.java
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class LRUDesign {
+
+    private static List<String> inputLine(Scanner sc) {
+        return Arrays.asList(sc.nextLine().split(" "));
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+
+        while (t-- > 0) {
+            int capacity = Integer.parseInt(sc.nextLine());
+            LRUCache cache = new LRUCache(capacity);
+
+            int queries = Integer.parseInt(sc.nextLine());
+            while (queries-- > 0) {
+                List<String> vec = inputLine(sc);
+                if (vec.get(0).equals("PUT")) {
+                    int key = Integer.parseInt(vec.get(1));
+                    int value = Integer.parseInt(vec.get(2));
+                    cache.put(key, value);
+                } else {
+                    int key = Integer.parseInt(vec.get(1));
+                    System.out.print(cache.get(key) + " ");
+                }
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// design the class in the most optimal way
+
+class LRUCache {
+    // Constructor for initializing the cache capacity with the given value.
+  Node head = new Node(0, 0), tail = new Node(0, 0);
+    Map < Integer, Node > map = new HashMap();
+    int capacity;
+
+    public LRUCache(int _capacity) {
+        capacity = _capacity;
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            remove(node);
+            insert(node);
+            return node.value;
+        } else {
+            return -1;
+        }
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            remove(map.get(key));
+        }
+        if (map.size() == capacity) {
+            remove(tail.prev);
+        }
+        insert(new Node(key, value));
+    }
+
+    private void remove(Node node) {
+        map.remove(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insert(Node node) {
+        map.put(node.key, node);
+        node.next = head.next;
+        node.next.prev = node;
+        head.next = node;
+        node.prev = head;
+    }
+
+    class Node {
+        Node prev, next;
+        int key, value;
+        Node(int _key, int _value) {
+            key = _key;
+            value = _value;
+        }
+    }
+}
