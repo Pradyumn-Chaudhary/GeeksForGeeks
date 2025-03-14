1f4360048b1475abe21864c1b7ee3324ae86bb3b
Pradyumn Chaudhary <155794678+Pradyumn-Chaudhary@users.noreply.github.com>
2025-03-15 00:00:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Introduction to Doubly Linked List/introduction-to-doubly-linked-list.java
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int x) {
+        data = x;
+        next = null;
+        prev = null;
+    }
+}
+
+class GFG {
+    public static void printList(Node node) {
+        Node tmp = node;
+        int c1 = 0, c2 = 0;
+        if (tmp != null) {
+            while (tmp.next != null) {
+                c1++;
+                tmp = tmp.next;
+            }
+            while (tmp.prev != null) {
+                c2++;
+                tmp = tmp.prev;
+            }
+            if (c1 != c2) {
+                System.out.println("-1");
+                return;
+            }
+        }
+        while (tmp != null) {
+            System.out.print(tmp.data + " ");
+            tmp = tmp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            Solution obj = new Solution();
+            Node ans = obj.constructDLL(arr);
+            printList(ans);
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    
+    Node constructDLL(int arr[]) {
+        // Code here
+        Node head = null;
+        Node prev = null;
+
+        for(int i : arr){
+            if(head == null){
+                head = new Node(i);
+                prev = head;
+                continue;
+            }
+            Node curr = new Node(i);
+            curr.prev = prev;
+            curr.next = null;
+            prev.next = curr;
+            prev = curr;
+        }
+        return head;
+    }
+}
